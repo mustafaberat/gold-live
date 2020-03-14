7fb6d78e3707d1d757eb89d64be8a78667749fd6
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-15 01:05:48 +0300

    first commit - react-navigation started

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import GoldPriceView from './src/view/gold-price'
+import MonthlyPageView from './src/view/monthly'
+import WeeklyPageView from './src/view/weekly'
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Monthly" component={MonthlyPageView} />
+        <Tab.Screen name="GoldPrice" component={GoldPriceView} />
+        <Tab.Screen name="Weekly" component={WeeklyPageView} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
