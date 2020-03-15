e2a994a7470090492c9274177cf3ffaddce34223
Mustafa Berat <32976555+mustafaberat@users.noreply.github.com>
2020-03-15 12:57:26 +0300

    add Styled-System

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
--- src/view/gold-price.js
+++ src/view/gold-price.js
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import { Text, View } from 'react-native'
 
+import Box from '../components/box'
+
 export default function GoldPriceView() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>gold-price!</Text>
+            <Box size={20} bg="blue" />
         </View>
     );
 }
